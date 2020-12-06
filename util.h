5a7eaaf049748cbf4e3ffae96f95f135935358e4
Shunsuke Mie <sux2mfgj@gmail.com>
2020-12-07 02:29:00 +0900

    [util] improve the NOT_YET_IMPLEMENTED macro.

--- util.h
+++ util.h
@@ -1,4 +1,4 @@
 #pragma once
 #include <err.h>
 
-#define NOT_YET_IMPLEMETED errx(1, "not yet implemented at line %d", __LINE__)
+#define NOT_YET_IMPLEMETED errx(1, "not yet implemented at line %d in %s", __LINE__, __FILE__)
