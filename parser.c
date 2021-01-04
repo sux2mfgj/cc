d810f0f0755dde4c03779746ddb1d4a333b03ba9
Shunsuke Mie <sux2mfgj@gmail.com>
2021-01-04 16:15:31 +0900

    add a parse_expr function.

--- parser.c
+++ parser.c
@@ -9,6 +9,40 @@
 // static node_t* parse_func_def(context_t* ctx, token_ctype_t* type,
 // token_id_t* id) { }
 
+static node_t* _parse_expr(context_t* ctx, node_t* left)
+{
+    token_t* t = get_front_token(ctx);
+
+    if(t->type == TK_OPR)
+    {
+        NOT_YET_IMPLEMETED;
+    }
+    else
+    {
+        return left;
+    }
+}
+
+static node_t* parse_expr(context_t* ctx)
+{
+    token_t* t = get_next_token(ctx);
+    switch(t->type)
+    {
+        case TK_NUM:
+        {
+            node_val_t* n = calloc(1, sizeof(node_val_t));
+            n->base.type = NODE_VAL;
+            n->uint64 = ((token_number_t*)t)->uint64;
+            return _parse_expr(ctx, (node_t*)n);
+        }
+        case TK_ID:
+        case TK_L_R_PAR:
+            NOT_YET_IMPLEMETED;
+        default:
+            errx(EXIT_FAILURE, "invalid token found");
+    }
+}
+
 static node_t* parse_single_node(context_t* ctx)
 {
     token_t* t = get_next_token(ctx);
@@ -50,7 +84,7 @@ static node_t* parse_var_def(context_t* ctx,
         return (node_t*)node;
     }
     else if (t->type == TK_ASSIGN) {
-        node->init = parse_single_node(ctx);
+        node->init = parse_expr(ctx);
         skip_semicolon(ctx);
         return (node_t*)node;
     }
